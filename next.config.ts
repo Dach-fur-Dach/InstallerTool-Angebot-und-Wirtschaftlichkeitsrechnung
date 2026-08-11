cd548024d897b3481d878894bfbbbc937c96e48a
P1a6 <pladdium@proton.me>
2026-08-11 14:52:03 +0200

    changing the root of the website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/partnertools",
   turbopack: {
     root: path.resolve(__dirname),
   },
